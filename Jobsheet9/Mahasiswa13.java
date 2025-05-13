c3d1a65f83589d2f5ab81c97fd27187315702a6f
Irfan Pandu Pratama <irfanpanduutama@gmail.com>
2025-05-13 20:40:56 +0700

    Jobsheet9

---
+++ Jobsheet9/Mahasiswa13.java
@@ -0,0 +1,19 @@
+
+public class Mahasiswa13 {
+    String nim, nama, kelas;
+    int nilai;
+
+    Mahasiswa13() {
+    }
+
+    Mahasiswa13(String nama, String nim, String kelas) {
+        this.nama = nama;
+        this.nim = nim;
+        this.kelas = kelas;
+        nilai = -1;
+    }
+
+    void tugasDinilai(int nilai) {
+        this.nilai = nilai;
+    }
+}
---
+++ Jobsheet9/StackTugasMahasiswa13.java
@@ -0,0 +1,104 @@
+public class StackTugasMahasiswa13{
+    Mahasiswa13
+    [] stack;
+    int size;
+    int top;
+
+    public StackTugasMahasiswa13
+    (int size) {
+        this.size = size;
+        stack = new Mahasiswa13
+        [size];
+        top = -1;
+    }
+
+    public boolean isFull() {
+        if (top == size - 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean isEmpty() {
+        if (top == -1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void push(Mahasiswa13
+     mhs) {
+        if (!isFull()) {
+            top++;
+            stack[top] = mhs;
+        } else {
+            System.out.println("Stack penuh! tidak bisa menambahkan tugas lagi!");
+        }
+    }
+
+    public Mahasiswa13
+     pop() {
+        if(!isEmpty()) {
+           Mahasiswa13
+            m = stack[top];
+            top--;
+            return m; 
+        } else {
+            System.out.println("Stack kosong! tidak ada tugas untuk dinilai");
+            return null;
+        }
+    } 
+
+    public Mahasiswa13
+     peek() {
+        if (!isEmpty()) {
+            return stack[top];
+        } else {
+            System.out.println("Stack kosong! Tidak ada tugas yang dikumpulkan");
+            return null;
+        }
+    }
+
+    public void print() {
+        for(int i = top; i >= 0; i--) {
+            System.out.println(stack[i].nama + "\t" + stack[i].nim + "\t" + stack[i].kelas);
+        }
+        System.out.println("");
+    }
+
+    public Mahasiswa13
+     peekBottom() {
+        if (!isEmpty()) {
+            return stack[0];
+        } else {
+            System.out.println("Stack kosong! Tidak ada tugas yang dikumpulkan");
+            return null;
+        }
+    }
+
+    public int jumlahTugas() {
+        if (!isEmpty()) {
+            return top + 1;
+        } else {
+            return 0;
+        }
+    }
+
+    public String konversiDesimalKeBiner(int nilai) {
+        StackKonversi13
+         stack = new StackKonversi13
+        ();
+        while (nilai > 0) {
+            int sisa = nilai % 2;
+            stack.push(sisa);
+            nilai = nilai / 2;
+        }
+        String biner = new String();
+        while (!stack.isEmpty()) {
+            biner += stack.pop();
+        }
+        return biner;
+    }
+}
